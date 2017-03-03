907724938f61f6363a1e82a6e7614c42b745431a
simo7 <simone.alessi@hotmail.com>
2017-03-03 16:14:06 +0100

    add hive deps for vagrant

--- setup/vagrant/provision.sh
+++ setup/vagrant/provision.sh
@@ -5,11 +5,19 @@ cd /opt/redash/current
 cp /opt/redash/.env /opt/redash/current
 bower install
 
+
+
 #install requirements
 sudo pip install -r /opt/redash/current/requirements_dev.txt
 sudo pip install -r /opt/redash/current/requirements.txt
-sudo pip install pymongo==3.2.1
-sudo pip install pyhive==0.1.6
+
+# in production use requirements_all_ds.txt instead
+sudo apt-get install libsasl2-dev
+sudo pip install sasl
+sudo pip install thrift
+sudo pip install thrift_sasl
+sudo pip install pyhive
+
 
 #update database
 bin/run ./manage.py database drop_tables
